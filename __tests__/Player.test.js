d713051fa24a6e5d09ac8dd99e1755edfbba6547
Zach Oda <zjodachow@gmail.com>
2022-08-02 15:40:47 -0400

    added testing to potions

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,5 +1,4 @@
 
-const { JestHook } = require("jest-watcher");
 const Player = require("../lib/Player");
 const Potion = require("../lib/Potion");
 
@@ -15,4 +14,24 @@ test('creates a player object', () => {
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
+  });
+
+
+
+  test("gets player's stats as an object", () => {
+    const player = new Player ("Dave");
+
+    expect(player.getStats()).toHaveProperty("potions");
+    expect(player.getStats()).toHaveProperty("health");
+    expect(player.getStats()).toHaveProperty("strength");
+    expect(player.getStats()).toHaveProperty("agility");
+  });
+
+
+  test("gets inventory from player or returns false", () => {
+    const player = new Player ("Dave");
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+    player.inventory = [];
+    expect(player.getInventory()).toEqual(false);
   });
